d6b7c98b6329482b0c5e26ff0e6fcc11b6358931
gpenverne <gpenverne@gmail.com>
2016-10-15 08:31:48 +0200

    statement

--- DependencyInjection/CloudflareExtension.php
+++ DependencyInjection/CloudflareExtension.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Gpenverne\Cloudflare\DependencyInjection;
+namespace Gpenverne\CloudflareBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
